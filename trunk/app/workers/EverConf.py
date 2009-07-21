1abc390a4fb82ea4640dbaee3a99f519f90c5fda
elliot.jaffe@gmail.com <elliot.jaffe@gmail.com@0c2027de-49d1-11de-b695-a305669b6508>
2009-07-21 21:21:23 +0000

    Add data collection of all slicestat fields except 1 and 15 minute bandwidth values

--- trunk/app/workers/EverConf.py
+++ trunk/app/workers/EverConf.py
@@ -22,7 +22,13 @@ SLICESTAT_SLICE_NAME_INDEX = 0 #Slice name
 SLICESTAT_CPU_INDEX = 2 #CPU consumption(%) 
 SLICESTAT_SEND_BW_INDEX = 8 #Average sending bandwidth for last 5 min(in Kbps)
 SLICESTAT_RECV_BW_INDEX = 11 #Average receiving bandwidth for last 5 min(in Kbps) 
+SLICESTAT_PCTMEM_INDEX = 3 # Percent memory used 
+SLICESTAT_PHYMEM_INDEX = 4 # Physical memory used
+SLICESTAT_VIRMEM_INDEX = 5 # Virtual memory used
+SLICESTAT_PROCS_INDEX = 6 # Allocated Processes
+SLICESTAT_RUNPROCS_INDEX = 14 # Running Processes 
 
+SLICESTAT_MAX_INDEX = 14 # last index that we care about
 
 DEF_SLICE_GROUP_ID = 1 #Default value for the slice group
 DAYS_TO_KEEP_SAMPLES_BACK = 5 #Default time to keep samples
