7ae334a9021528c484066a093b54f99392e35e0b
elliot.jaffe@gmail.com <elliot.jaffe@gmail.com@0c2027de-49d1-11de-b695-a305669b6508>
2009-09-22 14:19:08 +0000

    Update the data aggregation into dayusagesummaries.  Use them where possible and unify the topn pages

--- trunk/app/controllers/topn_controller.rb
+++ trunk/app/controllers/topn_controller.rb
@@ -2,15 +2,26 @@ class TopnController < ApplicationController
   layout 'default'
   @page_title = "Activity"
 
+  def doPieChart(title, pieValues, tooltip)
+    pie = Pie.new
+    pie.start_angle = 35
+    pie.animate = true
+    pie.tooltip = tooltip
+    pie.set_values(pieValues)
+    chart = OpenFlashChart.new
+    chart.set_title(title)
+    chart.add_element(pie)
+    return chart
+  end
+
   # GET /topn
   def index
     @item = { "count" => 5, "days" => 7 }
   end
 
-  # GET /topn/slices_by_cpu
-  # GET /topn/slices_by_cpu.xml
-  def slices_by_cpu
-    @page_title = "Activity: Top Slices by CPU Hours"
+  # GET /topn/nodes
+  def nodes
+    @page_title = "Activity: Top Nodes"
     @count = params[:count]
     @days = params[:days]
     if ! @count
@@ -20,173 +31,33 @@ class TopnController < ApplicationController
       @days = "7"
     end
 
-#    @dayusages = Dayusagesummary.find_by_sql("select 0 as id, slice_id as slice_id, max(nitems) as nitems, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW,  sum(total_send_BW) as total_send_BW, sum(total_cpu) as total_cpu, sum(total_send_BW) as total_send_BW, sum(total_recv_BW) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW, sum(number_of_samples) as number_of_samples from dayusagesummaries where day > date_sub(now(), interval " + @days +" day) and slice_id != -1 group by slice_id order by sum(total_cpu) desc limit " + @count)
+    @dayusages = Dayusagesummary.find_by_sql("select 0 as id, node_id as node_id, max(nitems) as nitems, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW,  sum(total_send_BW) as total_send_BW, sum(total_cpu) as total_cpu, sum(total_send_BW) as total_send_BW, sum(total_recv_BW) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW from dayusagesummaries where day > date_sub(now(), interval " + @days +" day) and slice_id = -1 and node_id != -1 group by node_id order by sum(total_CPU) desc limit " + @count)
 
-    @dayusages = Dayusage.find_by_sql("select 0 as id, slice_id as slice_id, count(distinct node_id) as nitems, now() as day, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW, sum(total_activity_minutes)*sum(total_cpu)/sum(number_of_samples)/100 as total_cpu, 60*sum(total_activity_minutes)*sum(total_send_BW)/sum(number_of_samples) as total_send_BW, 60*sum(total_activity_minutes)*sum(total_recv_BW)/sum(number_of_samples) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW, sum(number_of_samples) as number_of_samples, max(last_update) as last_update from dayusages where day > date_sub(now(), interval " + @days +" day) and slice_id != -1 and node_id != -1 group by slice_id order by sum(total_activity_minutes)*sum(total_cpu)/sum(number_of_samples) desc limit " + @count)
-
-    title = Title.new("Top %s slices by CPU for the past %s days" % [@count, @days])
-    pie = Pie.new
-    pie.start_angle = 35
-    pie.animate = true
-    pie.tooltip = '#label# - #val# CPU Hours'
-    vals = []
+    cpu = []
+    sendBW = []
+    recvBW = []
     @dayusages.each do |item|
-      vals << PieValue.new((item.total_cpu.to_f/60),item.slice.name)
+      cpu << PieValue.new(item.total_cpu,item.node.hostname)
+      sendBW << PieValue.new(item.total_send_BW,item.node.hostname)
+      recvBW << PieValue.new(item.total_recv_BW,item.node.hostname)
     end
-    pie.set_values(vals)
-    @chart = OpenFlashChart.new
-    @chart.set_title(title)
-    @chart.add_element(pie)
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @dayusages }
-    end
-  end
-
-  # GET /topn/slices_by_send_bw
-  # GET /topn/slices_by_send_bw.xml
-  def slices_by_send_bw
-    @page_title = "Activity: Top Slices by Sending Bandwidth"
-    @count = params[:count]
-    @days = params[:days]
-    if ! @count
-      @count = "5"
-    end
-    if ! @days
-      @days = "7"
-    end
-
-    @dayusages = Dayusage.find_by_sql("select 0 as id, slice_id, count(distinct node_id) as node_id, now() as day, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW, sum(total_activity_minutes)*sum(total_cpu)/sum(number_of_samples)/100 as total_cpu,  60*sum(total_activity_minutes)*sum(total_send_BW)/sum(number_of_samples) as total_send_BW, 60*sum(total_activity_minutes)*sum(total_recv_BW)/sum(number_of_samples) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW, sum(number_of_samples) as number_of_samples, max(last_update) as last_update from dayusages where day > date_sub(now(), interval " + @days +" day) group by slice_id order by  60*sum(total_activity_minutes)*sum(total_send_BW)/sum(number_of_samples) desc limit " + @count)
-
-    title = Title.new("Top %s slices by Total Sending Bandwidth for the past %s days" % [@count, @days])
-    pie = Pie.new
-    pie.start_angle = 35
-    pie.animate = true
-    pie.tooltip = '#label# - #val# Kb'
-    vals = []
-    @dayusages.each do |item|
-      vals << PieValue.new((item.total_send_BW.to_f/1024),item.slice.name)
-    end
-    pie.set_values(vals)
-    @chart = OpenFlashChart.new
-    @chart.set_title(title)
-    @chart.add_element(pie)
-
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @dayusages }
-    end
-  end
-
-  # GET /topn/slices_by_recv_bw
-  # GET /topn/slices_by_recv_bw.xml
-  def slices_by_recv_bw
-    @page_title = "Activity: Top Slices by Receiving Bandwidth"
-    @count = params[:count]
-    @days = params[:days]
-    if ! @count
-      @count = "5"
-    end
-    if ! @days
-      @days = "7"
-    end
-
-    @dayusages = Dayusage.find_by_sql("select 0 as id, slice_id, count(distinct node_id) as node_id, now() as day, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW, sum(total_activity_minutes)*sum(total_cpu)/sum(number_of_samples)/100 as total_cpu,  60*sum(total_activity_minutes)*sum(total_send_BW)/sum(number_of_samples) as total_send_BW, 60*sum(total_activity_minutes)*sum(total_recv_BW)/sum(number_of_samples) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW, sum(number_of_samples) as number_of_samples, max(last_update) as last_update from dayusages where day > date_sub(now(), interval " + @days +" day) group by slice_id order by 60*sum(total_activity_minutes)*sum(total_recv_BW)/sum(number_of_samples) desc limit " + @count)
-
-    title = Title.new("Top %s slices by Total Receiving Bandwidth for the past %s days" % [@count, @days])
-    pie = Pie.new
-    pie.start_angle = 35
-    pie.animate = true
-    pie.tooltip = '#label# - #val# Kb'
-    vals = []
-    @dayusages.each do |item|
-      vals << PieValue.new((item.total_recv_BW.to_f/1024),item.slice.name)
-    end
-    pie.set_values(vals)
-    @chart = OpenFlashChart.new
-    @chart.set_title(title)
-    @chart.add_element(pie)
-
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @dayusages }
-    end
-  end
-
-  # GET /topn/nodes_by_cpu
-  # GET /topn/nodes_by_cpu.xml
-  def nodes_by_cpu
-    @page_title = "Activity: Top Nodes by CPU Hours"
-    @count = params[:count]
-    @days = params[:days]
-    if ! @count
-      @count = "5"
-    end
-    if ! @days
-      @days = "7"
-    end
-
-    @dayusages = Dayusage.find_by_sql("select 0 as id, count(distinct slice_id)as slice_id, node_id, now() as day, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW, sum(total_activity_minutes)*sum(total_cpu)/sum(number_of_samples)/100 as total_cpu,  60*sum(total_activity_minutes)*sum(total_send_BW)/sum(number_of_samples) as total_send_BW, 60*sum(total_activity_minutes)*sum(total_recv_BW)/sum(number_of_samples) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW, sum(number_of_samples) as number_of_samples, max(last_update) as last_update from dayusages where day > date_sub(now(), interval " + @days +" day) group by node_id order by sum(total_activity_minutes)*sum(total_cpu)/sum(number_of_samples) desc limit " + @count)
-
     title = Title.new("Top %s nodes by CPU for the past %s days" % [@count, @days])
-    pie = Pie.new
-    pie.start_angle = 35
-    pie.animate = true
-    pie.tooltip = '#label# - #val# CPU Hours'
-    vals = []
-    @dayusages.each do |item|
-      vals << PieValue.new((item.total_cpu.to_f/60),item.node.hostname)
-    end
-    pie.set_values(vals)
-    @chart = OpenFlashChart.new
-    @chart.set_title(title)
-    @chart.add_element(pie)
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @dayusages }
-    end
-  end
-
-
-  # GET /topn/nodes_by_send_bw
-  # GET /topn/nodes_by_send_bw.xml
-  def nodes_by_send_bw
-    @page_title = "Activity: Top Nodes by Sending Bandwidth"
-    @count = params[:count]
-    @days = params[:days]
-    if ! @count
-      @count = "5"
-    end
-    if ! @days
-      @days = "7"
-    end
-
-    @dayusages = Dayusage.find_by_sql("select 0 as id, count(distinct slice_id)as slice_id, node_id, now() as day, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW, sum(total_activity_minutes)*sum(total_cpu)/sum(number_of_samples)/100 as total_cpu,  60*sum(total_activity_minutes)*sum(total_send_BW)/sum(number_of_samples) as total_send_BW, 60*sum(total_activity_minutes)*sum(total_recv_BW)/sum(number_of_samples) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW, sum(number_of_samples) as number_of_samples, max(last_update) as last_update from dayusages where day > date_sub(now(), interval " + @days +" day) group by node_id order by  60*sum(total_activity_minutes)*sum(total_send_BW)/sum(number_of_samples) desc limit " + @count)
-
+    @cpuchart = doPieChart(title, cpu, '#label# - #val# CPU Hours')
     title = Title.new("Top %s nodes by Total Sending Bandwidth for the past %s days" % [@count, @days])
-    pie = Pie.new
-    pie.start_angle = 35
-    pie.animate = true
-    pie.tooltip = '#label# - #val# Kb'
-    vals = []
-    @dayusages.each do |item|
-      vals << PieValue.new((item.total_send_BW.to_f/1024),item.node.hostname)
-    end
-    pie.set_values(vals)
-    @chart = OpenFlashChart.new
-    @chart.set_title(title)
-    @chart.add_element(pie)
+    @sendBWchart = doPieChart(title, sendBW, '#label# - #val# Kb')
+    title = Title.new("Top %s nodes by Total Incoming Bandwidth for the past %s days" % [@count, @days])
+    @recvBWchart = doPieChart(title, recvBW, '#label# - #val# Kb')
 
+    @charts = [@cpuchart, @sendBWchart, @recvBWchart]
     respond_to do |format|
       format.html
       format.xml  { render :xml => @dayusages }
     end
   end
 
-  # GET /topn/nodes_by_recv_bw
-  # GET /topn/nodes_by_recv_bw.xml
-  def nodes_by_recv_bw
-    @page_title = "Activity: Top Nodes by Receiving Bandwidth"
+  # GET /topn/slices
+  def slices
+    @page_title = "Activity: Top Slices"
     @count = params[:count]
     @days = params[:days]
     if ! @count
@@ -196,25 +67,28 @@ class TopnController < ApplicationController
       @days = "7"
     end
 
-    @dayusages = Dayusage.find_by_sql("select 0 as id, count(distinct slice_id)as slice_id, node_id, now() as day, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW, sum(total_activity_minutes)*sum(total_cpu)/sum(number_of_samples)/100 as total_cpu,  60*sum(total_activity_minutes)*sum(total_send_BW)/sum(number_of_samples) as total_send_BW, 60*sum(total_activity_minutes)*sum(total_recv_BW)/sum(number_of_samples) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW, sum(number_of_samples) as number_of_samples, max(last_update) as last_update from dayusages where day > date_sub(now(), interval " + @days +" day) group by node_id order by  60*sum(total_activity_minutes)*sum(total_recv_BW)/sum(number_of_samples) desc limit " + @count)
+    @dayusages = Dayusagesummary.find_by_sql("select 0 as id, slice_id as slice_id, max(nitems) as nitems, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW,  sum(total_send_BW) as total_send_BW, sum(total_cpu) as total_cpu, sum(total_send_BW) as total_send_BW, sum(total_recv_BW) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW from dayusagesummaries where day > date_sub(now(), interval " + @days +" day) and slice_id != -1 and node_id = -1 group by slice_id order by sum(total_cpu) desc limit " + @count)
 
-    title = Title.new("Top %s nodes by Total Receiving Bandwidth for the past %s days" % [@count, @days])
-    pie = Pie.new
-    pie.start_angle = 35
-    pie.animate = true
-    pie.tooltip = '#label# - #val# Kb'
-    vals = []
+    cpu = []
+    sendBW = []
+    recvBW = []
     @dayusages.each do |item|
-      vals << PieValue.new((item.total_recv_BW.to_f/1024),item.node.hostname)
+      cpu << PieValue.new(item.total_cpu,item.slice.name)
+      sendBW << PieValue.new(item.total_send_BW,item.slice.name)
+      recvBW << PieValue.new(item.total_recv_BW,item.slice.name)
     end
-    pie.set_values(vals)
-    @chart = OpenFlashChart.new
-    @chart.set_title(title)
-    @chart.add_element(pie)
+    title = Title.new("Top %s nodes by CPU for the past %s days" % [@count, @days])
+    @cpuchart = doPieChart(title, cpu, '#label# - #val# CPU Hours')
+    title = Title.new("Top %s nodes by Total Sending Bandwidth for the past %s days" % [@count, @days])
+    @sendBWchart = doPieChart(title, sendBW, '#label# - #val# Kb')
+    title = Title.new("Top %s nodes by Total Incoming Bandwidth for the past %s days" % [@count, @days])
+    @recvBWchart = doPieChart(title, recvBW, '#label# - #val# Kb')
 
+    @charts = [@cpuchart, @sendBWchart, @recvBWchart]
     respond_to do |format|
       format.html
       format.xml  { render :xml => @dayusages }
     end
   end
+
 end
