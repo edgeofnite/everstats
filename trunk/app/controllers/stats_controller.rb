bff295de3109db65ce8d3eddcbd6a360f5336a0a
elliot.jaffe@gmail.com <elliot.jaffe@gmail.com@0c2027de-49d1-11de-b695-a305669b6508>
2009-09-23 10:13:06 +0000

    Add xml and excel exporters to the stats/sliceview and stats/nodeview pages

--- trunk/app/controllers/stats_controller.rb
+++ trunk/app/controllers/stats_controller.rb
@@ -3,15 +3,18 @@ class StatsController < ApplicationController
 
     def nodeview
         @page_title = "Node Activity"
-#        basic_query_str = "SELECT node_id, count(distinct slice_id) as total_slices , SUM(total_activity_minutes) as total_activity_minutes, " +
-#                             "AVG(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW " +
-#                              "FROM dayusages "
 
-        basic_query_str = '''select 0 as id, node_id, avg(nitems) as nitems, SUM(total_activity_minutes) as total_activity_minutes, 
+        basic_query_str = '''select 0 as id, -1 as slice_id, node_id, curdate() as day, avg(number_of_samples) as number_of_samples,
+        avg(nitems) as nitems, SUM(total_activity_minutes) as total_activity_minutes, 
         sum(total_send_BW) as total_send_BW, sum(total_recv_BW) as total_recv_BW, 
         avg(avg_cpu) as avg_cpu, sum(total_cpu) as total_cpu,
         avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW,
-        avg(avg_procs) as avg_procs, avg(avg_runprocs) as avg_runprocs
+        avg(avg_procs) as avg_procs, max(max_procs) as max_procs,
+        avg(avg_runprocs) as avg_runprocs, max(max_runprocs) as max_runprocs,
+        max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW,
+        avg(avg_pctmem) as avg_pctmem, max(max_pctmem) as max_pctmem,
+        avg(avg_phymem) as avg_phymem, max(max_phymem) as max_phymem,
+        avg(avg_virmem) as avg_virmem, max(max_virmem) as max_virmem
         from dayusagesummaries where slice_id = -1 and node_id != -1 '''
 
         past_week_query_str = basic_query_str + "and day > date_add(curdate(), interval -7 day) GROUP BY node_id"
@@ -20,6 +23,23 @@ class StatsController < ApplicationController
         @past_week_node_usages = Dayusage.find_by_sql past_week_query_str
         @past_month_node_usages = Dayusage.find_by_sql past_month_query_str
         @past_year_node_usages = Dayusage.find_by_sql past_year_query_str
+
+    
+      respond_to do |format|
+        format.xml  { render :xml => @past_year_node_usages }
+        format.xls  do
+          e = Excel::Workbook.new
+          e.addWorksheetFromActiveRecord "past week", "Dayusagesummary", @past_week_node_usages
+          e.addWorksheetFromActiveRecord "past month", "Dayusagesummary", @past_month_node_usages
+          e.addWorksheetFromActiveRecord "past year", "Dayusagesummary", @past_year_node_usages
+          nodes = Node.find(:all)
+          e.addWorksheetFromActiveRecord "nodes", "Nodes", nodes
+          headers['Content-Type'] = 'application/vnd.ms-excel'
+          render :text => e.build
+        end
+        format.html
+      end
+
     end
 
     def sliceview
@@ -30,16 +50,20 @@ class StatsController < ApplicationController
             @slices_display = "all slices"
         end
         
-        basic_query_str = '''select 0 as id, slice_id, avg(nitems) as nitems, sum(total_send_BW) as total_send_BW, 
-	SUM(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, sum(total_cpu) as total_cpu,
+        basic_query_str = '''select 0 as id, -1 as node_id, curdate() as day, avg(number_of_samples) as number_of_samples, slice_id,
+        avg(nitems) as nitems, sum(total_send_BW) as total_send_BW, 
+	sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, sum(total_cpu) as total_cpu,
         sum(total_send_BW) as total_send_BW, sum(total_recv_BW) as total_recv_BW, 
         avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW,
-        avg(avg_procs) as avg_procs, avg(avg_runprocs) as avg_runprocs from dayusagesummaries,slices
+        avg(avg_procs) as avg_procs, max(max_procs) as max_procs,
+        avg(avg_runprocs) as avg_runprocs, max(max_runprocs) as max_runprocs,
+        max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW,
+        avg(avg_pctmem) as avg_pctmem, max(max_pctmem) as max_pctmem,
+        avg(avg_phymem) as avg_phymem, max(max_phymem) as max_phymem,
+        avg(avg_virmem) as avg_virmem, max(max_virmem) as max_virmem
+        from dayusagesummaries,slices
         where node_id = -1 and slice_id != -1  and dayusagesummaries.slice_id = slices.id '''
 
-#        basic_query_str = "SELECT slice_id,count(distinct node_id) as total_nodes , SUM(total_activity_minutes) as total_activity_minutes, " +
-#                        "AVG(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW " +
-#                       "FROM dayusages,slices where dayusages.slice_id = slices.id "
         if params["id"] 
             past_week_query_str = basic_query_str + "and day > date_add(curdate(), interval -7 day) and slices.slicegroup_id=" + params["id"] + " GROUP BY slice_id"
             past_month_query_str = basic_query_str + "and day > date_add(curdate(), interval -1 month) and slices.slicegroup_id=" + params["id"] + " GROUP BY slice_id"
@@ -50,10 +74,25 @@ class StatsController < ApplicationController
            past_year_query_str = basic_query_str + "and day > date_add(curdate(), interval -1 year) GROUP BY slice_id" 
           
         end
-        @past_week_usages = Dayusage.find_by_sql past_week_query_str
-        @past_month_usages = Dayusage.find_by_sql past_month_query_str
-        @past_year_usages = Dayusage.find_by_sql past_year_query_str
+        @past_week_usages = Dayusagesummary.find_by_sql past_week_query_str
+        @past_month_usages = Dayusagesummary.find_by_sql past_month_query_str
+        @past_year_usages = Dayusagesummary.find_by_sql past_year_query_str
     
+      respond_to do |format|
+        format.xml  { render :xml => @past_year_usages }
+        format.xls  do
+          e = Excel::Workbook.new
+          e.addWorksheetFromActiveRecord "past week", "Dayusagesummary", @past_week_usages
+          e.addWorksheetFromActiveRecord "past month", "Dayusagesummary", @past_month_usages
+          e.addWorksheetFromActiveRecord "past year", "Dayusagesummary", @past_year_usages
+          slices = Slice.find(:all)
+          e.addWorksheetFromActiveRecord "slices", "Slices", slices
+          headers['Content-Type'] = 'application/vnd.ms-excel'
+          render :text => e.build
+        end
+        format.html
+      end
+
     end
 
   def singlenodeview
