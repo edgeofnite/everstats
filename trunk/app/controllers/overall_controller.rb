3ff7e959324855ab8c4135923eb7cae9cba87bc7
elliot.jaffe@gmail.com <elliot.jaffe@gmail.com@0c2027de-49d1-11de-b695-a305669b6508>
2009-10-11 15:09:18 +0000

    Add support for selecting out particular slices.  Also export overall data as an Excel file

--- trunk/app/controllers/overall_controller.rb
+++ trunk/app/controllers/overall_controller.rb
@@ -58,6 +58,13 @@ class OverallController < ApplicationController
   end
 
   def byday
+    if params[:exclude_slices].nil? or params[:exclude_slices].empty?
+      excludestring = ""
+    else
+      excludesliceids = Slice.find(:all, :conditions => ['name like ?', params[:exclude_slices]]).map{|a| a.id}
+      excludestring = " and slice_id not in (#{excludesliceids.join(', ')})"
+    end
+
     startDay = params[:start_date][:year] + '-' + params[:start_date][:month] + '-' + params[:start_date][:day]
     endDay = params[:end_date][:year] + '-' + params[:end_date][:month] + '-' + params[:end_date][:day]
 
@@ -74,57 +81,87 @@ class OverallController < ApplicationController
     @totalCPU = Dayusagesummary.calculate(:sum, :total_cpu, :group => :day, :conditions => "slice_id = -1", :having => "day > '#{startDay}' and day < '#{endDay}'")
     @avgCPU = Dayusagesummary.calculate(:avg, :avg_cpu, :group => :day, :conditions => "slice_id = -1", :having => "day > '#{startDay}' and day < '#{endDay}'")
 
-    @avgSendBW = Dayusagesummary.calculate(:sum, :avg_send_BW, :group => :day, :conditions => "slice_id = -1", :having => "day > '#{startDay}' and day < '#{endDay}'")
-    @avgRecvBW = Dayusagesummary.calculate(:sum, :avg_recv_BW, :group => :day, :conditions => "slice_id = -1", :having => "day > '#{startDay}' and day < '#{endDay}'")
+    @avgSendBW = Dayusagesummary.calculate(:sum, :avg_send_BW, :group => :day, :conditions => "node_id = -1" + excludestring, :having => "day > '#{startDay}' and day < '#{endDay}'")
+    @avgRecvBW = Dayusagesummary.calculate(:sum, :avg_recv_BW, :group => :day, :conditions => "node_id = -1" + excludestring, :having => "day > '#{startDay}' and day < '#{endDay}'")
 
-    @totalSendBW = Dayusagesummary.calculate(:sum, :total_send_BW, :group => :day, :conditions => "slice_id = -1", :having => "day > '#{startDay}' and day < '#{endDay}'")
-    @totalRecvBW = Dayusagesummary.calculate(:sum, :total_recv_BW, :group => :day, :conditions => "slice_id = -1", :having => "day > '#{startDay}' and day < '#{endDay}'")
+    @totalSendBW = Dayusagesummary.calculate(:sum, :total_send_BW, :group => :day, :conditions => "node_id = -1" + excludestring, :having => "day > '#{startDay}' and day < '#{endDay}'")
+    @totalRecvBW = Dayusagesummary.calculate(:sum, :total_recv_BW, :group => :day, :conditions => "node_id = -1" + excludestring, :having => "day > '#{startDay}' and day < '#{endDay}'")
 
     @days = @nodes.keys
 
-    resources = {}
-    resources["nodes"] = []
-    resources["unique slices"] = []
-    resources["avg deployed slices"] = []
-    totalcpu = {}
-    totalcpu["Total CPU"] = []
-    avgcpu = {}
-    avgcpu["Average CPU"] = []
-    avgbw = {}
-    avgbw["Sending"] = []
-    avgbw["Receiving"] = []
-    totalbw = {}
-    totalbw["Sending"] = []
-    totalbw["Receiving"] = []
+    respond_to do |format|
+      format.html do
+      resources = {}
+      resources["nodes"] = []
+      resources["unique slices"] = []
+      resources["avg deployed slices"] = []
+      totalcpu = {}
+      totalcpu["Total CPU"] = []
+      avgcpu = {}
+      avgcpu["Average CPU"] = []
+      avgbw = {}
+      avgbw["Sending"] = []
+      avgbw["Receiving"] = []
+      totalbw = {}
+      totalbw["Sending"] = []
+      totalbw["Receiving"] = []
 
-    @days.each do |day|
-      resources["nodes"] << ScatterValue.new((day - startDayCivil), @nodes[day])
-      resources["unique slices"] << ScatterValue.new((day - startDayCivil), @slices[day])
-      resources["avg deployed slices"] << ScatterValue.new((day - startDayCivil), @deployedslices[day])
-      totalcpu["Total CPU"] << ScatterValue.new((day - startDayCivil), @totalCPU[day])
-      avgcpu["Average CPU"] << ScatterValue.new((day - startDayCivil), @avgCPU[day])
-      avgbw["Sending"] << ScatterValue.new((day - startDayCivil), @avgSendBW[day])
-      avgbw["Receiving"] << ScatterValue.new((day - startDayCivil), @avgRecvBW[day])
-      totalbw["Sending"] << ScatterValue.new((day - startDayCivil), @totalSendBW[day])
-      totalbw["Receiving"] << ScatterValue.new((day - startDayCivil), @totalRecvBW[day])
+      @days.each do |day|
+        resources["nodes"] << ScatterValue.new((day - startDayCivil), @nodes[day])
+        resources["unique slices"] << ScatterValue.new((day - startDayCivil), @slices[day])
+        resources["avg deployed slices"] << ScatterValue.new((day - startDayCivil), @deployedslices[day])
+        totalcpu["Total CPU"] << ScatterValue.new((day - startDayCivil), @totalCPU[day])
+        avgcpu["Average CPU"] << ScatterValue.new((day - startDayCivil), @avgCPU[day])
+        avgbw["Sending"] << ScatterValue.new((day - startDayCivil), @avgSendBW[day])
+        avgbw["Receiving"] << ScatterValue.new((day - startDayCivil), @avgRecvBW[day])
+        totalbw["Sending"] << ScatterValue.new((day - startDayCivil), @totalSendBW[day])
+        totalbw["Receiving"] << ScatterValue.new((day - startDayCivil), @totalRecvBW[day])
+      end
+
+      title = "Resources (Nodes and Slices) between %s and %s" % [startDayCivil, endDayCivil]
+      @resourcechart = doLineChart(title, "day", "Resources", resources, "#y# on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
+
+      title = "CPU Hours used between %s and %s" % [startDayCivil, endDayCivil]
+      @cpuchart = doLineChart(title, "day", "CPU Usage (Hours)", totalcpu, "#y# CPU Hours on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
+
+      title = "AVG CPU between %s and %s" % [startDayCivil, endDayCivil]
+      @avgcpuchart = doLineChart(title, "day", "Avg % CPU", avgcpu, "#y# Avg % CPU on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
+
+      title = "Average Network Bandwidth (KB)between %s and %s" % [startDayCivil, endDayCivil]
+      @avgbwchart = doLineChart(title, "day", "Network Bandwidth (KB)", avgbw, "#y# KB on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
+
+      title = "Total Network Bandwidth (KB) between %s and %s" % [startDayCivil, endDayCivil]
+      @totalbwchart = doLineChart(title, "day", "Network Bandwidth (KB)", totalbw, "#y# KB on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
+
+      @charts = [@resourcechart, @cpuchart, @avgcpuchart, @avgbwchart, @totalbwchart]
+      end
+      format.xls do
+        days = Array.new
+        @days.each do |day|
+          item = Hash.new
+          item["day"] = day
+          item["nodes"] = @nodes[day]
+          item["unique slices"] = @slices[day]
+          item["avg deployed slices"] = @deployedslices[day]
+          item["Total CPU"] = @totalCPU[day]
+          item["Average CPU"] = @avgCPU[day]
+          item["Sending"] = @avgSendBW[day]
+          item["Receiving"] =@avgRecvBW[day]
+          item["Sending"] = @totalSendBW[day]
+          item["Receiving"] = @totalRecvBW[day]
+          days << item
+        end
+
+        e = Excel::Workbook.new
+        e.addWorksheetFromArrayOfHashes "days", days
+        slices = Slice.find(:all)
+        e.addWorksheetFromActiveRecord "slices", "Slices", slices
+        nodes = Node.find(:all)
+        e.addWorksheetFromActiveRecord "nodes", "Nodes", nodes
+        headers['Content-Type'] = 'application/vnd.ms-excel'
+        render :text => e.build
+      end
     end
-
-    title = "Resources (Nodes and Slices) between %s and %s" % [startDayCivil, endDayCivil]
-    @resourcechart = doLineChart(title, "day", "Resources", resources, "#y# on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
-
-    title = "CPU Hours used between %s and %s" % [startDayCivil, endDayCivil]
-    @cpuchart = doLineChart(title, "day", "CPU Usage (Hours)", totalcpu, "#y# CPU Hours on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
-    
-    title = "AVG CPU between %s and %s" % [startDayCivil, endDayCivil]
-    @avgcpuchart = doLineChart(title, "day", "Avg % CPU", avgcpu, "#y# Avg % CPU on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
-
-    title = "Average Network Bandwidth (KB)between %s and %s" % [startDayCivil, endDayCivil]
-    @avgbwchart = doLineChart(title, "day", "Network Bandwidth (KB)", avgbw, "#y# KB on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
-    
-    title = "Total Network Bandwidth (KB) between %s and %s" % [startDayCivil, endDayCivil]
-    @totalbwchart = doLineChart(title, "day", "Network Bandwidth (KB)", totalbw, "#y# KB on day #x#",startDayTime.tv_sec, endDayTime.tv_sec)
-
-    @charts = [@resourcechart, @cpuchart, @avgcpuchart, @avgbwchart, @totalbwchart]
   end
 
 end
