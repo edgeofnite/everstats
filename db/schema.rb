00c57981bb0e7f0da8e9642c8d60c772a4e462ef
elliot.jaffe@gmail.com <elliot.jaffe@gmail.com@0c2027de-49d1-11de-b695-a305669b6508>
2009-11-05 12:45:19 +0000

    correct average bandwidth for slices (again)

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090914154548) do
+ActiveRecord::Schema.define(:version => 20091105124128) do
 
   create_table "configurations", :force => true do |t|
     t.string "config_key",   :limit => 20, :default => "", :null => false
