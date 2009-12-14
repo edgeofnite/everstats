1d2596888f6858b6869deaab67e956d7178c3371
elliot.jaffe <elliot.jaffe@0c2027de-49d1-11de-b695-a305669b6508>
2009-12-14 14:16:58 +0000

    fix calculations correctly this time.

--- trunk/app/controllers/node_controller.rb
+++ trunk/app/controllers/node_controller.rb
@@ -8,23 +8,11 @@ class NodeController < ApplicationController
   web_service_api NodeApi
   web_service_scaffold :invocation if Rails.env == 'development'
 
-  def upper_case(s)
-    s.upcase
-  end
-
-  def test_list(array)
-    result = ""
-    array.each do |i|
-      result += "%d - " % i
-    end
-    result
-  end
-
   def status(nodelist)
     result = []
     nodelist.each do |n|
       node = Node.find_by_hostname(n)
-      numslices = Dayusagesummary.calculate(:nitems, :conditions => "slice_id = -1 and node_id = #{node.id} and day = date(date_sub(now(), interval 1 day))'")
+      numslices = Dayusagesummary.maximum(:nitems, :conditions => "slice_id = -1 and node_id = #{node.id} and day = date(date_sub(now(), interval 1 day))")
       item = NodeStatus.new
       item.name = n
       item.numslices = numslices
