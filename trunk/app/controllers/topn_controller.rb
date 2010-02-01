e11544b84006092b49331d9aa209963a4bc10a5f
elliot.jaffe@gmail.com <elliot.jaffe@gmail.com@0c2027de-49d1-11de-b695-a305669b6508>
2010-02-01 10:24:19 +0000

    Fix sorting of topn slices.  It used to default to CPU, so if there was a mismatch, then the bandwidth items might not have appeared

--- trunk/app/controllers/topn_controller.rb
+++ trunk/app/controllers/topn_controller.rb
@@ -67,14 +67,20 @@ class TopnController < ApplicationController
       @days = "7"
     end
 
-    @dayusages = Dayusagesummary.find_by_sql("select 0 as id, slice_id as slice_id, max(nitems) as nitems, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW,  sum(total_send_BW) as total_send_BW, sum(total_cpu) as total_cpu, sum(total_send_BW) as total_send_BW, sum(total_recv_BW) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW from dayusagesummaries where day > date_sub(now(), interval " + @days +" day) and slice_id != -1 and node_id = -1 group by slice_id order by sum(total_cpu) desc limit " + @count)
+    @dayusages = Dayusagesummary.find_by_sql("select 0 as id, slice_id as slice_id, max(nitems) as nitems, sum(total_activity_minutes) as total_activity_minutes, avg(avg_cpu) as avg_cpu, avg(avg_send_BW) as avg_send_BW, avg(avg_recv_BW) as avg_recv_BW, sum(total_cpu) as total_cpu, sum(total_send_BW) as total_send_BW, sum(total_recv_BW) as total_recv_BW, max(max_cpu) as max_cpu, max(max_send_BW) as max_send_BW, max(max_recv_BW) as max_recv_BW from dayusagesummaries where day > date_sub(now(), interval " + @days +" day) and slice_id != -1 and node_id = -1 group by slice_id order by sum(total_cpu) desc limit " + @count)
+    @dayusagesSBW = Dayusagesummary.find_by_sql("select 0 as id, slice_id as slice_id, 0 as nitems, 0 as total_activity_minutes, 0 as avg_cpu, 0 as avg_send_BW, 0 as avg_recv_BW,  sum(total_send_BW) as total_send_BW, 0 as total_cpu, 0 as total_recv_BW, 0 as max_cpu, 0 as max_send_BW, 0 as max_recv_BW from dayusagesummaries where day > date_sub(now(), interval " + @days +" day) and slice_id != -1 and node_id = -1 group by slice_id order by sum(total_send_BW) desc limit " + @count)
+    @dayusagesSBW = Dayusagesummary.find_by_sql("select 0 as id, slice_id as slice_id, 0 as nitems, 0 as total_activity_minutes, 0 as avg_cpu, 0 as avg_send_BW, 0 as avg_recv_BW, 0 as total_send_BW, 0 as total_cpu, sum(total_recv_BW) as total_recv_BW, 0 as max_cpu, 0 as max_send_BW, 0 as max_recv_BW from dayusagesummaries where day > date_sub(now(), interval " + @days +" day) and slice_id != -1 and node_id = -1 group by slice_id order by sum(total_recv_BW) desc limit " + @count)
 
     cpu = []
     sendBW = []
     recvBW = []
     @dayusages.each do |item|
       cpu << PieValue.new(item.total_cpu,item.slice.name)
+    end
+    @dayusagesSBW.each do |item|
       sendBW << PieValue.new(item.total_send_BW,item.slice.name)
+    end
+    @dayusagesRBW.each do |item|
       recvBW << PieValue.new(item.total_recv_BW,item.slice.name)
     end
     title = Title.new("Top %s slices by CPU for the past %s days" % [@count, @days])
