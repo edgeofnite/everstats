580c260fe81b4bced995253be3ef49784b0d9432
elliot.jaffe@gmail.com <elliot.jaffe@gmail.com@0c2027de-49d1-11de-b695-a305669b6508>
2010-07-08 11:17:12 +0000

    Add support for CSV files.  Requires new gems

--- trunk/app/controllers/application_controller.rb
+++ trunk/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 require 'login_system'
+require 'comma'
 
 class ApplicationController < ActionController::Base
     include LoginSystem
--- trunk/app/models/dayusagesummary.rb
+++ trunk/app/models/dayusagesummary.rb
@@ -1,4 +1,33 @@
 class Dayusagesummary < ActiveRecord::Base
     belongs_to :node
     belongs_to :slice
+
+comma do
+  slice_id
+  node_id
+  day
+  nitems
+  total_activity_minutes
+  avg_cpu
+  avg_send_BW
+  avg_recv_BW
+  total_cpu
+  total_send_BW
+  total_recv_BW
+  max_cpu
+  max_send_BW
+  max_recv_BW
+  number_of_samples
+  avg_pctmem
+  max_pctmem
+  avg_phymem
+  max_phymem
+  avg_virmem
+  max_virmem
+  avg_procs
+  max_procs
+  avg_runprocs
+  max_runprocs
+end
+
 end
